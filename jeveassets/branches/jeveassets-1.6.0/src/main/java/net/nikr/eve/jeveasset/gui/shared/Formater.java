9cfec243f9fd95305f914eda424507e360cf9bc9
niklas <niklas@3b607cd1-60e7-68e3-df25-bc8014b3b472>
2010-11-24 13:14:26 +0000

    Creating 1.6.0 branch

---
+++ jeveassets/branches/jeveassets-1.6.0/src/main/java/net/nikr/eve/jeveasset/gui/shared/Formater.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2009, 2010 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.gui.shared;
+
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import net.nikr.eve.jeveasset.data.Settings;
+
+
+public class Formater {
+
+	private static DecimalFormat iskFormat  = new DecimalFormat("#,##0.00 isk");
+	private static DecimalFormat itemsFormat  = new DecimalFormat("#,##0 items");
+	private static DecimalFormat longFormat  = new DecimalFormat("#,##0");
+	private static DecimalFormat integerFormat  = new DecimalFormat("0");
+	private static DecimalFormat decimalFormat  = new DecimalFormat("#,##0.00");
+	private static DecimalFormat floatFormat  = new DecimalFormat("#,##0.####");
+	
+	private static DateFormat todaysDate = new SimpleDateFormat("yyyyMMdd", new Locale("en"));
+	private static DateFormat weekdayTime = new SimpleDateFormat("EEEEE HH:mm", new Locale("en"));
+	private static DateFormat timeOnly = new SimpleDateFormat("HH:mm", new Locale("en"));
+	private static DateFormat simpleDate = new SimpleDateFormat("yyyyMMddHHmm", new Locale("en"));
+
+	private Formater() {
+	}
+
+	public static String iskFormat(Double number){
+		return iskFormat.format(number);
+	}
+	public static String itemsFormat(Long number){
+		return itemsFormat.format(number);
+	}
+	public static String doubleFormat(Object obj){
+		return decimalFormat.format(obj);
+	}
+	/**
+	 * WARNING: This is not an good format for columns
+	 * It does however give a very precise result
+	 * 
+	 * @param obj value to be formated
+	 * @return formated value
+	 */
+	public static String floatFormat(Object obj){
+		return floatFormat.format(obj);
+	}
+	public static String integerFormat(Object obj){
+		return integerFormat.format(obj);
+	}
+	public static String longFormat(Object obj){
+		return longFormat.format(obj);
+	}
+
+	public static double round(double number, int decimalPlaces){
+		double modifier = Math.pow(10.0, decimalPlaces);
+		return Math.round(number * modifier) / modifier;
+	}
+
+	public static String weekdayAndTime(Date date){
+		if (today(date)){
+			return "Today "+timeOnly.format(date);
+		} else {
+			return weekdayTime.format(date);
+		}
+	}
+
+	public static String timeOnly(Date date){
+		return timeOnly.format(date);
+	}
+
+	public static String simpleDate(Date date){
+		return simpleDate.format(date);
+	}
+
+	private static boolean today(Date date){
+		String sDate = todaysDate.format(date);
+		String sNow = todaysDate.format(Settings.getGmtNow());
+		return sDate.equals(sNow);
+	}
+}
