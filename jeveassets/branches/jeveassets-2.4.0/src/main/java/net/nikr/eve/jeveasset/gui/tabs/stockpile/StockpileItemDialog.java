3891fa9eb86ce7789006eb8c69b56652be020d07
niklas <niklas@3b607cd1-60e7-68e3-df25-bc8014b3b472>
2012-06-25 12:19:40 +0000

    Creating branch for 2.4.0

---
+++ jeveassets/branches/jeveassets-2.4.0/src/main/java/net/nikr/eve/jeveasset/gui/tabs/stockpile/StockpileItemDialog.java
@@ -0,0 +1,313 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.gui.tabs.stockpile;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.TextFilterator;
+import ca.odell.glazedlists.swing.AutoCompleteSupport;
+import java.awt.Color;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import net.nikr.eve.jeveasset.Program;
+import net.nikr.eve.jeveasset.data.Item;
+import net.nikr.eve.jeveasset.gui.images.Images;
+import net.nikr.eve.jeveasset.gui.shared.components.JDialogCentered;
+import net.nikr.eve.jeveasset.gui.tabs.stockpile.Stockpile.StockpileItem;
+import net.nikr.eve.jeveasset.i18n.TabsStockpile;
+
+
+public class StockpileItemDialog extends JDialogCentered implements ActionListener, CaretListener, ItemListener {
+
+	private static final String ACTION_CANCEL = "ACTION_CANCEL";
+	private static final String ACTION_OK = "ACTION_OK";
+
+	private JButton jOK;
+	private JButton jCancel;
+	private JComboBox jItems;
+	private JTextField jCountMinimum;
+
+	private EventList<Item> items = new BasicEventList<Item>();
+	private Stockpile stockpile;
+	private StockpileItem stockpileItem;
+	private boolean updated = false;
+
+	public StockpileItemDialog(final Program program) {
+		super(program, TabsStockpile.get().addStockpileItem(), Images.TOOL_STOCKPILE.getImage());
+
+		JLabel jItemsLabel = new JLabel(TabsStockpile.get().item());
+		jItems = new JComboBox();
+		jItems.addItemListener(this);
+		AutoCompleteSupport<Item> itemAutoComplete = AutoCompleteSupport.install(jItems, items, new ItemFilterator());
+		itemAutoComplete.setStrict(true);
+		itemAutoComplete.setCorrectsCase(true);
+
+		JLabel jCountMinimumLabel = new JLabel(TabsStockpile.get().countMinimum());
+		jCountMinimum = new JTextField();
+		jCountMinimum.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(final FocusEvent e) {
+				jCountMinimum.selectAll();
+			}
+		});
+		jCountMinimum.addCaretListener(this);
+
+		jOK = new JButton(TabsStockpile.get().ok());
+		jOK.setActionCommand(ACTION_OK);
+		jOK.addActionListener(this);
+		jOK.setEnabled(false);
+
+		jCancel = new JButton(TabsStockpile.get().cancel());
+		jCancel.setActionCommand(ACTION_CANCEL);
+		jCancel.addActionListener(this);
+
+
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+				.addGroup(layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup()
+						.addComponent(jItemsLabel)
+						.addComponent(jCountMinimumLabel)
+					)
+					.addGroup(layout.createParallelGroup()
+						.addComponent(jItems, 300, 300, 300)
+						.addComponent(jCountMinimum, 300, 300, 300)
+					)
+				)
+				.addGroup(layout.createSequentialGroup()
+					.addComponent(jOK, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH)
+					.addComponent(jCancel, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH)
+				)
+		);
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup()
+					.addComponent(jItemsLabel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jItems, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(jCountMinimumLabel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jCountMinimum, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(jOK, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jCancel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+		);
+	}
+
+	boolean showEdit(final StockpileItem addStockpileItem) {
+		updateData();
+		this.stockpileItem = addStockpileItem;
+		this.getDialog().setTitle(TabsStockpile.get().editStockpileItem());
+		Item item = program.getSettings().getItems().get(addStockpileItem.getTypeID());
+		jItems.setSelectedItem(item);
+		jCountMinimum.setText(String.valueOf(addStockpileItem.getCountMinimum()));
+		show();
+		return updated;
+	}
+
+	boolean showAdd(final Stockpile addStockpile) {
+		updateData();
+		this.stockpile = addStockpile;
+		this.getDialog().setTitle(TabsStockpile.get().addStockpileItem());
+		show();
+		return updated;
+	}
+
+	boolean showAdd(final Stockpile addStockpile, final int typeID) {
+		updateData();
+		this.stockpile = addStockpile;
+		Item item = program.getSettings().getItems().get(typeID);
+		jItems.setSelectedItem(item);
+		jItems.setEnabled(false);
+		this.getDialog().setTitle(TabsStockpile.get().addStockpileItem());
+		show();
+		return updated;
+	}
+
+	private void updateData() {
+		stockpile = null;
+		stockpileItem = null;
+		List<Item> itemsList = new ArrayList<Item>(program.getSettings().getItems().values());
+		Collections.sort(itemsList);
+		try {
+			items.getReadWriteLock().writeLock().lock();
+			items.clear();
+			items.addAll(itemsList);
+		} finally {
+			items.getReadWriteLock().writeLock().unlock();
+		}
+		jItems.setSelectedIndex(0);
+		jCountMinimum.setText("");
+		jItems.setEnabled(true);
+	}
+
+	private void show() {
+		updated = false;
+		autoValidate();
+		autoSet();
+		super.setVisible(true);
+	}
+
+	private Stockpile getStockpile() {
+		if (stockpile != null) {
+			return stockpile;
+		} else {
+			return stockpileItem.getStockpile();
+		}
+	}
+
+	private StockpileItem getStockpileItem() {
+		Item item = (Item) jItems.getSelectedItem();
+		long countMinimum;
+		try {
+			countMinimum = Long.valueOf(jCountMinimum.getText());
+		} catch (NumberFormatException ex) {
+			countMinimum = 0;
+		}
+		return new StockpileItem(getStockpile(), item.getName(), item.getGroup(), item.getTypeID(), countMinimum);
+	}
+
+	private boolean itemExist() {
+		return getExistingItem() != null;
+	}
+
+	private StockpileItem getExistingItem() {
+		Item typeItem = (Item) jItems.getSelectedItem();
+		if (stockpile != null && typeItem != null) {
+			for (StockpileItem item : stockpile.getItems()) {
+				if (item.getTypeID() == typeItem.getTypeID()) {
+					return item;
+				}
+			}
+		}
+		return null;
+	}
+
+	private void autoValidate() {
+		boolean b = true;
+		boolean color = false;
+		if (jItems.getSelectedItem() == null) {
+			b = false;
+		}
+		if (itemExist() || stockpileItem != null) {
+			color = true;
+			jCountMinimum.setBackground(new Color(255, 255, 200));
+		}
+		try {
+			long l = Long.valueOf(jCountMinimum.getText());
+			if (l <= 0) {
+				b = false; //Negative and zero is not valid
+				color = true;
+				jCountMinimum.setBackground(new Color(255, 200, 200));
+			}
+		} catch (NumberFormatException ex) {
+			b = false; //Empty and NaN is not valid
+			if (!jCountMinimum.getText().isEmpty()) {
+				color = true;
+				jCountMinimum.setBackground(new Color(255, 200, 200));
+			}
+		}
+		if (!color) {
+			jCountMinimum.setBackground(Color.WHITE);
+		}
+		jOK.setEnabled(b);
+	}
+
+	private void autoSet() {
+		final StockpileItem item = getExistingItem();
+		if (item != null) {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					jCountMinimum.setText(String.valueOf(item.getCountMinimum()));
+				}
+			});
+		}
+	}
+
+	@Override
+	protected JComponent getDefaultFocus() {
+		if (jItems.isEnabled()) {
+			return jItems;
+		} else {
+			return jCountMinimum;
+		}
+	}
+
+	@Override
+	protected JButton getDefaultButton() {
+		return jOK;
+	}
+
+	@Override
+	protected void windowShown() { }
+
+	@Override
+	protected void save() {
+		if (stockpileItem != null) { //EDIT
+			stockpile = getStockpile();
+			stockpile.remove(stockpileItem);
+		}
+		if (itemExist()) { //UPDATING (Adding an existing item)
+			stockpile.remove(getExistingItem());
+		}
+		//ADD & EDIT & UPDATING
+		stockpile.add(getStockpileItem());
+		updated = true;
+		super.setVisible(false);
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		if (ACTION_OK.equals(e.getActionCommand())) {
+			save();
+		}
+		if (ACTION_CANCEL.equals(e.getActionCommand())) {
+			this.setVisible(false);
+		}
+	}
+
+	@Override
+	public void caretUpdate(final CaretEvent e) {
+		autoValidate();
+	}
+
+	@Override
+	public void itemStateChanged(final ItemEvent e) {
+		autoValidate();
+		autoSet();
+	}
+
+	static class ItemFilterator implements TextFilterator<Item> {
+		@Override
+		public void getFilterStrings(final List<String> baseList, final Item element) {
+			baseList.add(element.getName());
+		}
+	}
+}
---
+++ jeveassets/branches/jeveassets-2.4.0/src/test/java/net/nikr/eve/jeveasset/i18n/TestI18N.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.i18n;
+
+
+import net.nikr.eve.jeveasset.Main;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ *
+ * @author Candle
+ */
+public class TestI18N {
+	
+	@Test
+	public void testDataModelAssetBundle_en() throws Exception {
+		DataModelEveAsset g = Main.getBundleService().get(DataModelEveAsset.class);
+		assertNotNull(g.priceBuyAvg());
+	}
+
+	@Test
+	public void testDataModelIndustryJobBundle_en() throws Exception {
+		DataModelIndustryJob g = Main.getBundleService().get(DataModelIndustryJob.class);
+		assertNotNull(g.activityAll());
+	}
+
+	@Test
+	public void testDataModelPriceDataSettings_en() throws Exception {
+		DataModelPriceDataSettings g = Main.getBundleService().get(DataModelPriceDataSettings.class);
+		assertNotNull(g.regionDerelik());
+	}
+
+	@Test
+	public void testDialoguesAbout_en() throws Exception {
+		DialoguesAbout g = Main.getBundleService().get(DialoguesAbout.class);
+		assertNotNull(g.about());
+	}
+
+	@Test
+	public void testDialoguesAccount_en() throws Exception {
+		DialoguesAccount g = Main.getBundleService().get(DialoguesAccount.class);
+		assertNotNull(g.helpText());
+	}
+
+	@Test
+	public void testDialoguesAddSystem_en() throws Exception {
+		DialoguesAddSystem g = Main.getBundleService().get(DialoguesAddSystem.class);
+		assertNotNull(g.filterResult(5));
+	}
+
+	@Test
+	public void testDialoguesCsvExport_en() throws Exception {
+		DialoguesCsvExport g = Main.getBundleService().get(DialoguesCsvExport.class);
+		assertNotNull(g.noFilter());
+	}
+
+	@Test
+	public void testDialoguesProfiles_en() throws Exception {
+		DialoguesProfiles g = Main.getBundleService().get(DialoguesProfiles.class);
+		assertNotNull(g.deleteProfileConfirm("delete me!"));
+	}
+
+	@Test
+	public void testDialoguesSettings_en() throws Exception {
+		DialoguesSettings g = Main.getBundleService().get(DialoguesSettings.class);
+		assertNotNull(g.enterFilter());
+	}
+
+	@Test
+	public void testDialoguesUpdate_en() throws Exception {
+		DialoguesUpdate g = Main.getBundleService().get(DialoguesUpdate.class);
+		assertNotNull(g.accountBlances());
+	}
+
+	@Test public void testGeneralBundle_en() throws Exception {
+		General g = Main.getBundleService().get(General.class);
+		assertNotNull(g.uncaughtErrorMessage());
+	}
+
+	@Test public void testGuiFrameBundle_en() throws Exception {
+		GuiFrame g = Main.getBundleService().get(GuiFrame.class);
+		assertNotNull(g.about());
+	}
+
+	@Test public void testGuiSharedBundle_en() throws Exception {
+		GuiShared g = Main.getBundleService().get(GuiShared.class);
+		assertNotNull(g.add());
+	}
+
+	@Test public void testTabsAssetsBundle_en() throws Exception {
+		TabsAssets g = Main.getBundleService().get(TabsAssets.class);
+		assertNotNull(g.assets());
+	}
+
+	@Test public void testTabsItemsBundle_en() throws Exception {
+		TabsItems g = Main.getBundleService().get(TabsItems.class);
+		assertNotNull(g.columnName());
+	}
+
+	@Test public void testTabsJobsBundle_en() throws Exception {
+		TabsJobs g = Main.getBundleService().get(TabsJobs.class);
+		assertNotNull(g.all());
+	}
+
+	@Test public void testTabsLoadoutBundle_en() throws Exception {
+		TabsLoadout g = Main.getBundleService().get(TabsLoadout.class);
+		assertNotNull(g.cancel());
+	}
+
+	@Test public void testTabsMaterialsBundle_en() throws Exception {
+		TabsMaterials g = Main.getBundleService().get(TabsMaterials.class);
+		assertNotNull(g.collapse());
+	}
+
+	@Test public void testTabsOrdersBundle_en() throws Exception {
+		TabsOrders g = Main.getBundleService().get(TabsOrders.class);
+		assertNotNull(g.columnExpires());
+	}
+
+	@Test public void testTabsOverviewBundle_en() throws Exception {
+		TabsOverview g = Main.getBundleService().get(TabsOverview.class);
+		assertNotNull(g.add());
+	}
+
+	@Test public void testTabsRoutingBundle_en() throws Exception {
+		TabsRouting g = Main.getBundleService().get(TabsRouting.class);
+		assertNotNull(g.add());
+	}
+
+	@Test public void testTabsStockpileBundle_en() throws Exception {
+		TabsStockpile g = Main.getBundleService().get(TabsStockpile.class);
+		assertNotNull(g.addItem());
+	}
+
+	@Test public void testTabsValuesBundle_en() throws Exception {
+		TabsValues g = Main.getBundleService().get(TabsValues.class);
+		assertNotNull(g.assets());
+	}
+}
