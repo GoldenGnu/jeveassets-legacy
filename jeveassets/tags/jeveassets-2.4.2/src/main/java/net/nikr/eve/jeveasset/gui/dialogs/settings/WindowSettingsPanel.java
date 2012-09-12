2b3b180333a3c0408a72bf9d104ae734ba3b1a81
niklas <niklas@3b607cd1-60e7-68e3-df25-bc8014b3b472>
2012-09-12 12:25:47 +0000

    Tagging 2.4.2

---
+++ jeveassets/tags/jeveassets-2.4.2/src/main/java/net/nikr/eve/jeveasset/gui/dialogs/settings/WindowSettingsPanel.java
@@ -0,0 +1,245 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.gui.dialogs.settings;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import net.nikr.eve.jeveasset.Program;
+import net.nikr.eve.jeveasset.gui.images.Images;
+import net.nikr.eve.jeveasset.gui.shared.components.JNumberField;
+import net.nikr.eve.jeveasset.i18n.DialoguesSettings;
+
+
+public class WindowSettingsPanel extends JSettingsPanel implements ActionListener {
+
+	private static final String ACTION_AUTO_SAVE = "ACTION_AUTO_SAVE";
+	private static final String ACTION_FIXED = "ACTION_FIXED";
+	private static final String ACTION_DEFAULT = "ACTION_DEFAULT";
+
+	public static final int SAVE_ON_EXIT = 1;
+	public static final int FLAG_SAVE_MAXIMIZED = 2;
+	public static final int FLAG_SAVE_FIXED = 3;
+
+	private JRadioButton jAutoSave;
+	private JRadioButton jFixed;
+	private JTextField jWidth;
+	private JTextField jHeight;
+	private JTextField jX;
+	private JTextField jY;
+	private JCheckBox jMaximized;
+	private JCheckBox jAlwaysOnTop;
+	private JButton jDefault;
+
+	public WindowSettingsPanel(final Program program, final SettingsDialog optionsDialog) {
+		super(program, optionsDialog, DialoguesSettings.get().windowWindow(), Images.SETTINGS_WINDOW.getIcon());
+
+		jAutoSave = new JRadioButton(DialoguesSettings.get().windowSaveOnExit());
+		jAutoSave.setActionCommand(ACTION_AUTO_SAVE);
+		jAutoSave.addActionListener(this);
+
+		jFixed = new JRadioButton(DialoguesSettings.get().windowFixed());
+		jFixed.setActionCommand(ACTION_FIXED);
+		jFixed.addActionListener(this);
+
+		JLabel jWidthLabel = new JLabel(DialoguesSettings.get().windowWidth());
+		jWidth = new JNumberField();
+
+		JLabel jHeightLabel = new JLabel(DialoguesSettings.get().windowHeight());
+		jHeight = new JNumberField();
+
+		JLabel jXLabel = new JLabel(DialoguesSettings.get().windowX());
+		jX = new JNumberField();
+
+		JLabel jYLabel = new JLabel(DialoguesSettings.get().windowY());
+		jY = new JNumberField();
+
+		jMaximized = new JCheckBox(DialoguesSettings.get().windowMaximised());
+
+		jAlwaysOnTop = new JCheckBox(DialoguesSettings.get().windowAlwaysOnTop());
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(jAutoSave);
+		group.add(jFixed);
+
+
+		jDefault = new JButton("Default");
+		jDefault.setActionCommand(ACTION_DEFAULT);
+		jDefault.addActionListener(this);
+
+
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(jAlwaysOnTop)
+				.addComponent(jAutoSave)
+				.addComponent(jFixed)
+				.addGroup(layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+						.addComponent(jWidthLabel)
+						.addComponent(jHeightLabel)
+					)
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(jWidth)
+						.addComponent(jHeight)
+					)
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+						.addComponent(jXLabel)
+						.addComponent(jYLabel)
+					)
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+						.addComponent(jX)
+						.addComponent(jY)
+						.addComponent(jDefault)
+					)
+				)
+				.addComponent(jMaximized)
+		);
+
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addComponent(jAlwaysOnTop, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				.addComponent(jAutoSave, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				.addComponent(jFixed, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+					.addComponent(jWidthLabel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jWidth, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jXLabel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jX, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+					.addComponent(jHeightLabel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jHeight, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jYLabel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jY, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+					.addComponent(jMaximized, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jDefault, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+		);
+	}
+
+	private void setValuesFromSettings() {
+		jWidth.setText(String.valueOf(program.getMainWindow().getFrame().getSize().width));
+		jHeight.setText(String.valueOf(program.getMainWindow().getFrame().getSize().height));
+		jX.setText(String.valueOf(program.getMainWindow().getFrame().getLocation().x));
+		jY.setText(String.valueOf(program.getMainWindow().getFrame().getLocation().y));
+		jMaximized.setSelected(program.getMainWindow().getFrame().getState() == JFrame.MAXIMIZED_BOTH);
+	}
+
+	private void setValuesFromWindow() {
+		jWidth.setText(String.valueOf(program.getSettings().getWindowSize().width));
+		jHeight.setText(String.valueOf(program.getSettings().getWindowSize().height));
+		jX.setText(String.valueOf(program.getSettings().getWindowLocation().x));
+		jY.setText(String.valueOf(program.getSettings().getWindowLocation().y));
+		jMaximized.setSelected(program.getSettings().isWindowMaximized());
+	}
+
+	private int validate(final String s) {
+		int n;
+		try {
+			n = Integer.valueOf(s);
+		} catch (NumberFormatException ex) {
+			n = 0;
+		}
+		if (n < 0) {
+			n = 0;
+		}
+		return n;
+	}
+
+	private void setInputEnabled(final boolean b) {
+		jWidth.setEnabled(b);
+		jHeight.setEnabled(b);
+		jX.setEnabled(b);
+		jY.setEnabled(b);
+		jMaximized.setEnabled(b);
+		jDefault.setEnabled(b);
+	}
+
+	@Override
+	public boolean save() {
+		if (jAutoSave.isSelected()) {
+			program.getSettings().setWindowAutoSave(true);
+		} else {
+			int width = validate(jWidth.getText());
+			int height = validate(jHeight.getText());
+			int x = validate(jX.getText());
+			int y = validate(jY.getText());
+			boolean maximized = jMaximized.isSelected();
+			Dimension d = new Dimension(width, height);
+			Point p = new Point(x, y);
+			boolean first = program.getSettings().isWindowAutoSave();
+			program.getSettings().setWindowAutoSave(false);
+
+			//if changed...
+			if ((program.getSettings().getWindowSize().height != d.height)
+					|| (program.getSettings().getWindowSize().width != d.width)
+					|| (program.getSettings().getWindowLocation().x != p.x)
+					|| (program.getSettings().getWindowLocation().y != p.y)
+					|| (program.getSettings().isWindowMaximized() != maximized)
+					|| first) {
+				program.getSettings().setWindowSize(d);
+				program.getSettings().setWindowLocation(p);
+				program.getSettings().setWindowMaximized(maximized);
+				program.getMainWindow().setSizeAndLocation(d, p, maximized);
+			}
+		}
+		boolean alwaysOnTop = jAlwaysOnTop.isSelected();
+		program.getSettings().setWindowAlwaysOnTop(alwaysOnTop);
+		program.getMainWindow().getFrame().setAlwaysOnTop(alwaysOnTop);
+		return false;
+	}
+
+	@Override
+	public void load() {
+		if (program.getSettings().isWindowAutoSave()) {
+			jAutoSave.setSelected(true);
+			setValuesFromSettings();
+			setInputEnabled(false);
+		} else {
+			jFixed.setSelected(true);
+			setValuesFromWindow();
+			setInputEnabled(true);
+		}
+		jAlwaysOnTop.setSelected(program.getSettings().isWindowAlwaysOnTop());
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		if (ACTION_AUTO_SAVE.equals(e.getActionCommand())) {
+			setInputEnabled(false);
+		}
+		if (ACTION_FIXED.equals(e.getActionCommand())) {
+			setInputEnabled(true);
+		}
+		if (ACTION_DEFAULT.equals(e.getActionCommand())) {
+			jWidth.setText("800");
+			jHeight.setText("600");
+			jX.setText("0");
+			jY.setText("0");
+		}
+	}
+
+}
---
+++ jeveassets/tags/jeveassets-2.4.2/src/main/java/net/nikr/eve/jeveasset/io/shared/ApiConverter.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.io.shared;
+
+import com.beimin.eveapi.shared.assetlist.EveAsset;
+import com.beimin.eveapi.shared.industryjobs.ApiIndustryJob;
+import com.beimin.eveapi.shared.marketorders.ApiMarketOrder;
+import java.util.ArrayList;
+import java.util.List;
+import net.nikr.eve.jeveasset.data.*;
+import net.nikr.eve.jeveasset.i18n.General;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ApiConverter {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ApiConverter.class);;
+
+	private ApiConverter() { }
+
+	public static List<Asset> apiMarketOrder(final List<ApiMarketOrder> marketOrders, final Human human, final Settings settings) {
+		List<Asset> eveAssets = new ArrayList<Asset>();
+		for (ApiMarketOrder apiMarketOrder : marketOrders) {
+			if (apiMarketOrder.getOrderState() == 0 && apiMarketOrder.getVolRemaining() > 0
+					&& ((apiMarketOrder.getBid() < 1 && settings.isIncludeSellOrders())
+					|| (apiMarketOrder.getBid() > 0 && settings.isIncludeBuyOrders()))
+					) {
+				Asset eveAsset = apiMarketOrderToEveAsset(apiMarketOrder, human, settings);
+				eveAssets.add(eveAsset);
+			}
+		}
+		return eveAssets;
+	}
+
+	private static Asset apiMarketOrderToEveAsset(final ApiMarketOrder apiMarketOrder, final Human human, final Settings settings) {
+		int typeID = apiMarketOrder.getTypeID();
+		long locationID = apiMarketOrder.getStationID();
+		long count = apiMarketOrder.getVolRemaining();
+		long itemId = apiMarketOrder.getOrderID();
+		String flag;
+		if (apiMarketOrder.getBid() < 1) { //Sell
+			flag = General.get().marketOrderSellFlag();
+		} else { //Buy
+			flag = General.get().marketOrderBuyFlag();
+		}
+		int flagID = 0;
+		boolean corporation = human.isCorporation();
+		boolean singleton  = true;
+		int rawQuantity = 0;
+
+		//Calculated:
+		String name = ApiIdConverter.typeName(typeID, settings.getItems());
+		String group = ApiIdConverter.group(typeID, settings.getItems());
+		String category = ApiIdConverter.category(typeID, settings.getItems());
+		double basePrice = ApiIdConverter.priceBase(typeID, settings.getItems());
+		boolean marketGroup = ApiIdConverter.marketGroup(typeID, settings.getItems());
+		float volume = ApiIdConverter.volume(typeID, settings.getItems());
+		int meta = ApiIdConverter.meta(typeID, settings.getItems());
+		String tech = ApiIdConverter.tech(typeID, settings.getItems());
+		String owner = human.getName();
+		String location = ApiIdConverter.locationName(locationID, null, settings.getLocations());
+		String region = ApiIdConverter.regionName(locationID, null, settings.getLocations());
+		String security = ApiIdConverter.security(locationID, null, settings.getLocations());
+		String solarSystem = ApiIdConverter.systemName(locationID, null, settings.getLocations());
+		long solarSystemId  = ApiIdConverter.systemID(locationID, null, settings.getLocations());
+		List<Asset> parents = new ArrayList<Asset>();
+		boolean piMaterial = ApiIdConverter.piMaterial(typeID, settings.getItems());
+
+		return new Asset(name, group, category, owner, count, location, parents, flag, flagID, basePrice, meta, tech, itemId, typeID, marketGroup, corporation, volume, region, locationID, singleton, security, solarSystem, solarSystemId, rawQuantity, piMaterial);
+	}
+
+	public static List<Asset> apiIndustryJob(final List<ApiIndustryJob> industryJobs, final Human human, final Settings settings) {
+		List<Asset> eveAssets = new ArrayList<Asset>();
+		for (ApiIndustryJob apiIndustryJob : industryJobs) {
+			if (!apiIndustryJob.isCompleted()) {
+				Asset eveAsset = apiIndustryJobToEveAsset(apiIndustryJob, human, settings);
+				eveAssets.add(eveAsset);
+			}
+		}
+		return eveAssets;
+	}
+
+	private static Asset apiIndustryJobToEveAsset(final ApiIndustryJob apiIndustryJob, final Human human, final Settings settings) {
+		int typeID = apiIndustryJob.getInstalledItemTypeID();
+		long locationID = apiIndustryJobLocationId(apiIndustryJob, settings);
+		long count = apiIndustryJob.getInstalledItemQuantity();
+		long id = apiIndustryJob.getInstalledItemID();
+		int flagID = apiIndustryJob.getInstalledItemFlag();
+		boolean corporation = human.isCorporation();
+		boolean singleton  = false;
+		int rawQuantity;
+		if (apiIndustryJob.getInstalledItemCopy() == 0) {
+			rawQuantity = 0; //0 = BPO
+		} else {
+			rawQuantity = -2; //-2 = BPC
+		}
+
+		//Calculated:
+		String name = ApiIdConverter.typeName(typeID, settings.getItems());
+		String group = ApiIdConverter.group(typeID, settings.getItems());
+		String category = ApiIdConverter.category(typeID, settings.getItems());
+		double basePrice = ApiIdConverter.priceBase(typeID, settings.getItems());
+		boolean marketGroup = ApiIdConverter.marketGroup(typeID, settings.getItems());
+		float volume = ApiIdConverter.volume(typeID, settings.getItems());
+		int meta = ApiIdConverter.meta(typeID, settings.getItems());
+		String tech = ApiIdConverter.tech(typeID, settings.getItems());
+		String owner = human.getName();
+		String location = ApiIdConverter.locationName(locationID, null, settings.getLocations());
+		String region = ApiIdConverter.regionName(locationID, null, settings.getLocations());
+		String security = ApiIdConverter.security(locationID, null, settings.getLocations());
+		String solarSystem = ApiIdConverter.systemName(locationID, null, settings.getLocations());
+		long solarSystemId  = ApiIdConverter.systemID(locationID, null, settings.getLocations());
+		List<Asset> parents = new ArrayList<Asset>();
+		String flag = ApiIdConverter.flag(flagID, null, settings.getItemFlags());
+		boolean piMaterial = ApiIdConverter.piMaterial(typeID, settings.getItems());
+
+		return new Asset(name, group, category, owner, count, location, parents, flag, flagID, basePrice, meta, tech, id, typeID, marketGroup, corporation, volume, region, locationID, singleton, security, solarSystem, solarSystemId, rawQuantity, piMaterial);
+	}
+
+	public static List<Asset> apiAsset(final Human human, final List<EveAsset<?>> assets, final Settings settings) {
+		List<Asset> eveAssets = new ArrayList<Asset>();
+		apiAsset(human, assets, eveAssets, null, settings);
+		return eveAssets;
+	}
+	private static void apiAsset(final Human human, final List<EveAsset<?>> assets, final List<Asset> eveAssets, final Asset parentEveAsset, final Settings settings) {
+		for (EveAsset<?> asset : assets) {
+			Asset eveAsset = apiAssetsToEveAsset(human, asset, parentEveAsset, settings);
+			if (parentEveAsset == null) {
+				eveAssets.add(eveAsset);
+			} else {
+				parentEveAsset.addEveAsset(eveAsset);
+			}
+			apiAsset(human, new ArrayList<EveAsset<?>>(asset.getAssets()), eveAssets, eveAsset, settings);
+		}
+	}
+	private static Asset apiAssetsToEveAsset(final Human human, final EveAsset<?> apiAsset, final Asset parentEveAsset, final Settings settings) {
+		long count = apiAsset.getQuantity();
+		int flagID = apiAsset.getFlag();
+		long itemId = apiAsset.getItemID();
+		int typeID = apiAsset.getTypeID();
+		long locationID;
+		if (apiAsset.getLocationID() != null) { //Top level
+			locationID = apiAsset.getLocationID();
+		} else if (parentEveAsset != null) { //Sub level
+			locationID = parentEveAsset.getLocationID();
+		} else { //Fail (fallback)
+			locationID = 0;
+		}
+		boolean singleton  = apiAsset.getSingleton();
+		boolean corporation = human.isCorporation();
+		String owner = human.getName();
+		int rawQuantity = apiAsset.getRawQuantity();
+		//Calculated:
+		String name = ApiIdConverter.typeName(apiAsset.getTypeID(), settings.getItems());
+		String group = ApiIdConverter.group(apiAsset.getTypeID(), settings.getItems());
+		String category = ApiIdConverter.category(apiAsset.getTypeID(), settings.getItems());
+		double basePrice = ApiIdConverter.priceBase(apiAsset.getTypeID(), settings.getItems());
+		int meta = ApiIdConverter.meta(typeID, settings.getItems());
+		String tech = ApiIdConverter.tech(typeID, settings.getItems());
+		boolean marketGroup = ApiIdConverter.marketGroup(apiAsset.getTypeID(), settings.getItems());
+		float volume = ApiIdConverter.volume(apiAsset.getTypeID(), settings.getItems());
+		String security = ApiIdConverter.security(locationID, parentEveAsset, settings.getLocations());
+		String region = ApiIdConverter.regionName(locationID, parentEveAsset, settings.getLocations());
+		String location = ApiIdConverter.locationName(locationID, parentEveAsset, settings.getLocations());
+		String solarSystem = ApiIdConverter.systemName(locationID, parentEveAsset, settings.getLocations());
+		long solarSystemId  = ApiIdConverter.systemID(locationID, parentEveAsset, settings.getLocations());
+		List<Asset> parents = ApiIdConverter.parents(parentEveAsset);
+		String flag = ApiIdConverter.flag(flagID, parentEveAsset, settings.getItemFlags());
+		boolean piMaterial = ApiIdConverter.piMaterial(typeID, settings.getItems());
+
+		return new Asset(name, group, category, owner, count, location, parents, flag, flagID, basePrice, meta, tech, itemId, typeID, marketGroup, corporation, volume, region, locationID, singleton, security, solarSystem, solarSystemId, rawQuantity, piMaterial);
+	}
+	public static List<MarketOrder> apiMarketOrdersToMarketOrders(final Human human, final List<ApiMarketOrder> apiMarketOrders, final Settings settings) {
+		List<MarketOrder> marketOrders = new ArrayList<MarketOrder>();
+		for (ApiMarketOrder apiMarketOrder : apiMarketOrders) {
+			marketOrders.add(apiMarketOrderToMarketOrder(human, apiMarketOrder, settings));
+		}
+		return marketOrders;
+	}
+	private static MarketOrder apiMarketOrderToMarketOrder(final Human human, final ApiMarketOrder apiMarketOrder, final Settings settings) {
+		String name = ApiIdConverter.typeName(apiMarketOrder.getTypeID(), settings.getItems());
+		String location = ApiIdConverter.locationName(apiMarketOrder.getStationID(), null, settings.getLocations());
+		String system = ApiIdConverter.systemName(apiMarketOrder.getStationID(), null, settings.getLocations());
+		String region = ApiIdConverter.regionName(apiMarketOrder.getStationID(), null, settings.getLocations());
+		String owner = human.getName();
+		return new MarketOrder(apiMarketOrder, name, location, system, region, owner);
+	}
+	public static List<IndustryJob> apiIndustryJobsToIndustryJobs(final List<ApiIndustryJob> apiIndustryJobs, final String owner, final Settings settings) {
+		List<IndustryJob> industryJobs = new ArrayList<IndustryJob>();
+		for (ApiIndustryJob apiIndustryJob : apiIndustryJobs) {
+			industryJobs.add(apiIndustryJobToIndustryJob(apiIndustryJob, owner, settings));
+		}
+		return industryJobs;
+	}
+
+	private static IndustryJob apiIndustryJobToIndustryJob(final ApiIndustryJob apiIndustryJob, final String owner, final Settings settings) {
+		String name = ApiIdConverter.typeName(apiIndustryJob.getInstalledItemTypeID(), settings.getItems());
+		long locationID = apiIndustryJobLocationId(apiIndustryJob, settings);
+		String location = ApiIdConverter.locationName(locationID, null, settings.getLocations());
+		String system = ApiIdConverter.systemName(locationID, null, settings.getLocations());
+		String region = ApiIdConverter.regionName(locationID, null, settings.getLocations());
+		return new IndustryJob(apiIndustryJob, name, location, system, region, owner);
+	}
+
+	private static long apiIndustryJobLocationId(final ApiIndustryJob apiIndustryJob, final Settings settings) {
+		boolean location = ApiIdConverter.locationTest(apiIndustryJob.getInstalledItemLocationID(), null, settings.getLocations());
+		if (location) {
+			return apiIndustryJob.getInstalledItemLocationID();
+		}
+		location = ApiIdConverter.locationTest(apiIndustryJob.getContainerLocationID(), null, settings.getLocations());
+		if (location) {
+			return apiIndustryJob.getContainerLocationID();
+		}
+		LOG.error("Failed to find locationID for IndustryJob. InstalledItemLocationID: " + apiIndustryJob.getInstalledItemLocationID() + " - ContainerLocationID: " + apiIndustryJob.getContainerLocationID());
+		return -1;
+	}
+}
