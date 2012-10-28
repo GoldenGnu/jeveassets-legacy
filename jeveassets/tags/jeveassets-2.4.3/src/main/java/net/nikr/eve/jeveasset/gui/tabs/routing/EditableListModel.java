a33d87ead60ac3a1b5cd19c62aff7ed9ef8f1932
niklaskr@gmail.com <niklaskr@gmail.com@3b607cd1-60e7-68e3-df25-bc8014b3b472>
2012-10-28 14:00:44 +0000

    Tagging 2.4.3

---
+++ jeveassets/tags/jeveassets-2.4.3/src/main/java/net/nikr/eve/jeveasset/gui/tabs/routing/EditableListModel.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.gui.tabs.routing;
+
+import java.util.*;
+import javax.swing.AbstractListModel;
+import javax.swing.SwingUtilities;
+
+public class EditableListModel<T> extends AbstractListModel {
+
+	private static final long serialVersionUID = 1L;
+	private List<T> backed = new ArrayList<T>();
+	private Comparator<T> sortComparator = new Comparator<T>() {
+		@Override
+		public int compare(final T o1, final T o2) {
+			return o2.hashCode() - o1.hashCode();
+		}
+	};
+
+	public EditableListModel() {
+	}
+
+	public EditableListModel(final List<T> initial) {
+		backed.addAll(initial);
+	}
+
+	public EditableListModel(final List<T> initial, final Comparator<T> sortComparator) {
+		backed.addAll(initial);
+		this.sortComparator = sortComparator;
+	}
+
+	public void setSortComparator(final Comparator<T> sortComparator) {
+		this.sortComparator = sortComparator;
+	}
+
+	public Comparator<T> getSortComparator() {
+		return sortComparator;
+	}
+
+	public List<? extends T> getAll() {
+		return Collections.unmodifiableList(backed);
+	}
+
+	@Override
+	public int getSize() {
+		return backed.size();
+	}
+
+	@Override
+	public Object getElementAt(final int index) {
+		return backed.get(index);
+	}
+
+	public T remove(final int index) {
+		T b = backed.remove(index);
+		changed();
+		return b;
+	}
+
+	public void clear() {
+		backed.clear();
+		changed();
+	}
+
+	public boolean remove(final T o) {
+		boolean b = backed.remove(o);
+		changed();
+		return b;
+	}
+
+	public boolean add(final T e) {
+		boolean b = backed.add(e);
+		Collections.sort(backed, sortComparator);
+		changed();
+		return b;
+	}
+
+	public boolean addAll(final Collection<? extends T> c) {
+		boolean b = backed.addAll(c);
+		changed();
+		return b;
+	}
+
+	public boolean contains(final T o) {
+		return backed.contains(o);
+	}
+
+	void changed() {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				fireContentsChanged(this, 0, backed.size() - 1);
+			}
+		});
+	}
+}
---
+++ jeveassets/tags/jeveassets-2.4.3/src/main/java/net/nikr/eve/jeveasset/i18n/TabsRouting.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2009, 2010, 2011, 2012 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.i18n;
+
+import java.util.Locale;
+import net.nikr.eve.jeveasset.Main;
+import uk.me.candle.translations.Bundle;
+
+public abstract class TabsRouting extends Bundle {
+	public static TabsRouting get() {
+		return Main.getBundleService().get(TabsRouting.class);
+	}
+
+	public TabsRouting(final Locale locale) {
+		super(locale);
+	}
+
+	public abstract String a(Object arg0, Object arg1);
+	public abstract String add();
+	public abstract String all();
+	public abstract String algorithm();
+	public abstract String calculate();
+	public abstract String cancel();
+	public abstract String error();
+	public abstract String filteredAssets();
+	public abstract String generating();
+	public abstract String not();
+	public abstract String once();
+	public abstract String overviewGroup(Object arg0);
+	public abstract String route();
+	public abstract String route1();
+	public abstract String routing();
+	public abstract String routing1();
+	public abstract String second(int time);
+	public abstract String source();
+	public abstract String there();
+	public abstract String unknown(Object arg0);
+	public abstract String whitespace();
+	public abstract String whitespace1();
+	public abstract String whitespace2(Object arg0, Object arg1);
+	public abstract String whitespace3(Object arg0, Object arg1);
+	public abstract String whitespace4(Object arg0, Object arg1);
+	public abstract String whitespace5();
+}
