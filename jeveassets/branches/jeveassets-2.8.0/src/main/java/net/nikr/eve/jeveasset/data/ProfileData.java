29cb292fc78936f86afbed24cbf01bec761e895d
niklaskr <niklaskr@3b607cd1-60e7-68e3-df25-bc8014b3b472>
2014-01-27 11:57:32 +0000

    minor optimization

--- jeveassets/branches/jeveassets-2.8.0/src/main/java/net/nikr/eve/jeveasset/data/ProfileData.java
+++ jeveassets/branches/jeveassets-2.8.0/src/main/java/net/nikr/eve/jeveasset/data/ProfileData.java
@@ -483,7 +483,8 @@ public class ProfileData {
 			asset.setContainer(sContainer);
 
 			//Price data
-			if (asset.getItem().isMarketGroup() && Settings.get().getPriceData().containsKey(asset.getItem().getTypeID()) && !Settings.get().getPriceData().get(asset.getItem().getTypeID()).isEmpty()) { //Market Price
+			PriceData priceData = Settings.get().getPriceData().get(asset.getItem().getTypeID());
+			if (asset.getItem().isMarketGroup() && priceData != null && !priceData.isEmpty()) { //Market Price
 				asset.setPriceData(Settings.get().getPriceData().get(asset.getItem().getTypeID()));
 			} else { //No Price :(
 				asset.setPriceData(null);
