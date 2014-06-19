02e49e44968716ef61a3bbe90dd21e633c875b67
niklaskr <niklaskr@3b607cd1-60e7-68e3-df25-bc8014b3b472>
2014-06-19 10:48:01 +0000

    Tagging 2.8.5

---
+++ jeveassets/tags/jeveassets-2.8.5/src/main/java/net/nikr/eve/jeveasset/gui/shared/filter/FilterSave.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2009-2014 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.gui.shared.filter;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.TextFilterator;
+import ca.odell.glazedlists.swing.AutoCompleteSupport;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.text.JTextComponent;
+import net.nikr.eve.jeveasset.Program;
+import net.nikr.eve.jeveasset.gui.shared.CaseInsensitiveComparator;
+import net.nikr.eve.jeveasset.gui.shared.components.JCopyPopup;
+import net.nikr.eve.jeveasset.gui.shared.components.JDialogCentered;
+import net.nikr.eve.jeveasset.i18n.GuiShared;
+
+public class FilterSave extends JDialogCentered {
+
+	private enum FilterSaveAction {
+		SAVE, CANCEL
+	}
+
+	private final EventList<String> filters;
+	private final List<String> defaultFilters = new ArrayList<String>();
+	private final JComboBox jName;
+	private final JButton jSave;
+
+	private String returnString;
+
+	public FilterSave(final JFrame jFrame) {
+		super(null, GuiShared.get().saveFilter(), jFrame);
+
+		ListenerClass listener = new ListenerClass();
+
+		JLabel jText = new JLabel(GuiShared.get().enterFilterName());
+
+		jName = new JComboBox();
+		JCopyPopup.install((JTextComponent) jName.getEditor().getEditorComponent());
+		filters = new BasicEventList<String>();
+		AutoCompleteSupport.install(jName, filters, new Filterator());
+		jSave = new JButton(GuiShared.get().save());
+		jSave.setActionCommand(FilterSaveAction.SAVE.name());
+		jSave.addActionListener(listener);
+
+		JButton jCancel = new JButton(GuiShared.get().cancel());
+		jCancel.setActionCommand(FilterSaveAction.CANCEL.name());
+		jCancel.addActionListener(listener);
+
+		layout.setHorizontalGroup(
+			layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(jText)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+					.addComponent(jName, 220, 220, 220)
+					.addGroup(layout.createSequentialGroup()
+						.addComponent(jSave, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH)
+						.addComponent(jCancel, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH, Program.BUTTONS_WIDTH)
+					)
+				)
+		);
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addComponent(jText, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				.addComponent(jName, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(jSave, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+					.addComponent(jCancel, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT, Program.BUTTONS_HEIGHT)
+				)
+		);
+	}
+
+	String show(final List<String> filters, final List<String> defaultFilters) {
+		returnString = null;
+		Collections.sort(filters, new CaseInsensitiveComparator());
+		try {
+			this.filters.getReadWriteLock().writeLock().lock();
+			this.filters.clear();
+			this.filters.addAll(filters);
+		} finally {
+			this.filters.getReadWriteLock().writeLock().unlock();
+		}
+		this.defaultFilters.clear();
+		this.defaultFilters.addAll(defaultFilters);
+		this.setVisible(true);
+		return returnString;
+	}
+
+	private boolean validate() {
+		String name = (String) jName.getSelectedItem();
+		if (name == null) {
+			JOptionPane.showMessageDialog(this.getDialog(), GuiShared.get().noFilterName(), GuiShared.get().saveFilter(), JOptionPane.PLAIN_MESSAGE);
+			return false;
+		}
+		if (name.isEmpty()) {
+			JOptionPane.showMessageDialog(this.getDialog(), GuiShared.get().noFilterName(), GuiShared.get().saveFilter(), JOptionPane.PLAIN_MESSAGE);
+			return false;
+		}
+		for (String filter : defaultFilters) {
+			if (filter.toLowerCase().equals(name.toLowerCase())) { //Case insetitive contains
+				JOptionPane.showMessageDialog(this.getDialog(), GuiShared.get().overwriteDefaultFilter(), GuiShared.get().saveFilter(), JOptionPane.PLAIN_MESSAGE);
+				return false;
+			}
+		}
+		if (filters.contains(name)) {
+			int nReturn = JOptionPane.showConfirmDialog(this.getDialog(), GuiShared.get().overwrite(), GuiShared.get().overwriteFilter(), JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if (nReturn == JOptionPane.NO_OPTION) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	protected JComponent getDefaultFocus() {
+		return jName;
+	}
+
+	@Override
+	protected JButton getDefaultButton() {
+		return jSave;
+	}
+
+	@Override
+	protected void windowShown() { }
+
+	@Override
+	protected void save() {
+		if (validate()) {
+			returnString = (String) jName.getSelectedItem();
+			setVisible(false);
+		}
+		//XXX - Workaround for strange bug:
+		// 1. Tricker validation by pressing enter in the jName JComboBox
+		// 2. Doing validate JOptionPane is shown and lose focus (to another program)
+		// 3. JOptionPane is hidden (by mouse click)
+		// 4. jName is not responding (string is locked)
+		try {
+			Robot robot = new Robot();
+			robot.keyRelease(KeyEvent.VK_ENTER);
+		} catch (AWTException e) {
+
+		}
+	}
+
+	@Override
+	public void setVisible(final boolean b) {
+		if (b) {
+			jName.getModel().setSelectedItem("");
+		}
+		super.setVisible(b);
+	}
+
+	private class ListenerClass implements ActionListener {
+		@Override
+		public void actionPerformed(final ActionEvent e) {
+			if (FilterSaveAction.SAVE.name().equals(e.getActionCommand())) {
+				save();
+			}
+			if (FilterSaveAction.CANCEL.name().equals(e.getActionCommand())) {
+				setVisible(false);
+			}
+		}
+	}
+
+	private static class Filterator implements TextFilterator<String> {
+		@Override
+		public void getFilterStrings(final List<String> baseList, final String element) {
+			if (element.length() > 0) {
+				baseList.add(element);
+			}
+		}
+	}
+}
---
+++ jeveassets/tags/jeveassets-2.8.5/src/main/java/net/nikr/eve/jeveasset/gui/tabs/values/ValueTableTab.java
@@ -0,0 +1,279 @@
+/*
+ * Copyright 2009-2014 Contributors (see credits.txt)
+ *
+ * This file is part of jEveAssets.
+ *
+ * jEveAssets is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * jEveAssets is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jEveAssets; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package net.nikr.eve.jeveasset.gui.tabs.values;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.FilterList;
+import ca.odell.glazedlists.ListSelection;
+import ca.odell.glazedlists.SortedList;
+import ca.odell.glazedlists.swing.DefaultEventSelectionModel;
+import ca.odell.glazedlists.swing.DefaultEventTableModel;
+import ca.odell.glazedlists.swing.TableComparatorChooser;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JScrollPane;
+import net.nikr.eve.jeveasset.Program;
+import net.nikr.eve.jeveasset.data.MyAccountBalance;
+import net.nikr.eve.jeveasset.data.Settings;
+import net.nikr.eve.jeveasset.gui.images.Images;
+import net.nikr.eve.jeveasset.gui.shared.components.JMainTab;
+import net.nikr.eve.jeveasset.gui.shared.filter.Filter;
+import net.nikr.eve.jeveasset.gui.shared.filter.FilterControl;
+import net.nikr.eve.jeveasset.gui.shared.menu.MenuData;
+import net.nikr.eve.jeveasset.gui.shared.menu.MenuManager.TableMenu;
+import net.nikr.eve.jeveasset.gui.shared.table.EnumTableColumn;
+import net.nikr.eve.jeveasset.gui.shared.table.EnumTableFormatAdaptor;
+import net.nikr.eve.jeveasset.gui.shared.table.EventModels;
+import net.nikr.eve.jeveasset.gui.shared.table.JAutoColumnTable;
+import net.nikr.eve.jeveasset.gui.shared.table.PaddingTableCellRenderer;
+import net.nikr.eve.jeveasset.gui.tabs.assets.MyAsset;
+import net.nikr.eve.jeveasset.gui.tabs.jobs.MyIndustryJob;
+import net.nikr.eve.jeveasset.gui.tabs.orders.MyMarketOrder;
+import net.nikr.eve.jeveasset.i18n.General;
+import net.nikr.eve.jeveasset.i18n.TabsValues;
+import net.nikr.eve.jeveasset.io.shared.ApiIdConverter;
+
+
+public class ValueTableTab extends JMainTab {
+
+	//GUI
+	private final JAutoColumnTable jTable;
+
+	//Table
+	private final ValueFilterControl filterControl;
+	private final DefaultEventTableModel<Value> tableModel;
+	private final EventList<Value> eventList;
+	private final FilterList<Value> filterList;
+	private final EnumTableFormatAdaptor<ValueTableFormat, Value> tableFormat;
+	private final DefaultEventSelectionModel<Value> selectionModel;
+
+	public static final String NAME = "value"; //Not to be changed!
+
+	public ValueTableTab(final Program program) {
+		super(program, TabsValues.get().title(), Images.TOOL_VALUE_TABLE.getIcon(), true);
+		//Table Format
+		tableFormat = new EnumTableFormatAdaptor<ValueTableFormat, Value>(ValueTableFormat.class);
+		//Backend
+		eventList = new BasicEventList<Value>();
+		//Sorting (per column)
+		SortedList<Value> columnSortedList = new SortedList<Value>(eventList);
+		//Sorting Total
+		SortedList<Value> totalSortedList = new SortedList<Value>(columnSortedList, new TotalComparator());
+		//Filter
+		filterList = new FilterList<Value>(totalSortedList);
+		//Table Model
+		tableModel = EventModels.createTableModel(filterList, tableFormat);
+		//Table
+		jTable = new JValueTable(program, tableModel);
+		jTable.setCellSelectionEnabled(true);
+		jTable.setRowSelectionAllowed(true);
+		jTable.setColumnSelectionAllowed(true);
+		PaddingTableCellRenderer.install(jTable, 3);
+		//Sorting
+		TableComparatorChooser.install(jTable, columnSortedList, TableComparatorChooser.MULTIPLE_COLUMN_MOUSE, tableFormat);
+		//Selection Model
+		selectionModel = EventModels.createSelectionModel(filterList);
+		selectionModel.setSelectionMode(ListSelection.MULTIPLE_INTERVAL_SELECTION_DEFENSIVE);
+		jTable.setSelectionModel(selectionModel);
+		//Listeners
+		installTable(jTable, NAME);
+		//Scroll
+		JScrollPane jTableScroll = new JScrollPane(jTable);
+		//Table Filter
+		filterControl = new ValueFilterControl(
+				program.getMainWindow().getFrame(),
+				tableFormat,
+				totalSortedList,
+				filterList,
+				Settings.get().getTableFilters(NAME)
+				);
+
+		//Menu
+		installMenu(program, new ValueTableMenu(), jTable, Value.class);
+
+		layout.setHorizontalGroup(
+			layout.createParallelGroup()
+				.addComponent(filterControl.getPanel())
+				.addComponent(jTableScroll, 0, 0, Short.MAX_VALUE)
+		);
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addComponent(filterControl.getPanel())
+				.addComponent(jTableScroll, 0, 0, Short.MAX_VALUE)
+		);
+	}
+
+	private Value getValue(Map<String, Value> values, String owner) {
+		Value value = values.get(owner);
+		if (value == null) {
+			value = new Value(owner);
+			values.put(owner, value);
+		}
+		return value;
+	}
+
+	@Override
+	public void updateData() {
+		Map<String, Value> values = new HashMap<String, Value>();
+		Value total = new Value(TabsValues.get().grandTotal());
+		values.put(total.getName(), total);
+		for (MyAsset asset : program.getAssetEventList()) {
+			//Skip market orders
+			if (asset.getFlag().equals(General.get().marketOrderSellFlag())) {
+				continue; //Ignore market sell orders
+			}
+			if (asset.getFlag().equals(General.get().marketOrderBuyFlag())) {
+				continue; //Ignore market buy orders
+			}
+			//Skip contracts
+			if (asset.getFlag().equals(General.get().contractIncluded())) {
+				continue; //Ignore contracts included
+			}
+			if (asset.getFlag().equals(General.get().contractExcluded())) {
+				continue; //Ignore contracts excluded
+			}
+			Value value = getValue(values, asset.getOwner());
+			value.addAssets(asset);
+			total.addAssets(asset);
+		}
+		//Account Balance
+		for (MyAccountBalance accountBalance : program.getAccountBalanceEventList()) {
+			Value value = getValue(values, accountBalance.getOwner());
+			value.addBalance(accountBalance.getBalance());
+			total.addBalance(accountBalance.getBalance());
+		}
+		//Market Orders
+		for (MyMarketOrder marketOrder : program.getMarketOrdersEventList()) {
+			Value value = getValue(values, marketOrder.getOwner());
+			if (marketOrder.getOrderState() == 0) {
+				if (marketOrder.getBid() < 1) { //Sell Orders
+					value.addSellOrders(marketOrder.getPrice() * marketOrder.getVolRemaining());
+					total.addSellOrders(marketOrder.getPrice() * marketOrder.getVolRemaining());
+				} else { //Buy Orders
+					value.addEscrows(marketOrder.getEscrow());
+					value.addEscrowsToCover((marketOrder.getPrice() * marketOrder.getVolRemaining()) - marketOrder.getEscrow());
+					total.addEscrows(marketOrder.getEscrow());
+					total.addEscrowsToCover((marketOrder.getPrice() * marketOrder.getVolRemaining()) - marketOrder.getEscrow());
+				}
+			}
+		}
+		//Industrys Job: Manufacturing
+		for (MyIndustryJob industryJob : program.getIndustryJobsEventList()) {
+			Value value = getValue(values, industryJob.getOwner());
+			//Manufacturing and not completed
+			if (industryJob.getActivity() == MyIndustryJob.IndustryActivity.ACTIVITY_MANUFACTURING && !industryJob.isCompleted()) {
+				double manufacturingTotal = industryJob.getPortion() * industryJob.getRuns() * ApiIdConverter.getPrice(industryJob.getOutputTypeID(), false);
+				value.addManufacturing(manufacturingTotal);
+				total.addManufacturing(manufacturingTotal);
+			}
+		}
+		try {
+			eventList.getReadWriteLock().writeLock().lock();
+			eventList.clear();
+			eventList.addAll(values.values());
+		} finally {
+			eventList.getReadWriteLock().writeLock().unlock();
+		}
+	}
+
+	private class ValueTableMenu implements TableMenu<Value> {
+		@Override
+		public MenuData<Value> getMenuData() {
+			return new MenuData<Value>();
+		}
+
+		@Override
+		public JMenu getFilterMenu() {
+			return filterControl.getMenu(jTable, selectionModel.getSelected());
+		}
+
+		@Override
+		public JMenu getColumnMenu() {
+			return tableFormat.getMenu(program, tableModel, jTable, NAME);
+		}
+
+		@Override
+		public void addInfoMenu(JComponent jComponent) { }
+
+		@Override
+		public void addToolMenu(JComponent jComponent) { }
+	}
+
+	private class ValueFilterControl extends FilterControl<Value> {
+
+		private final EnumTableFormatAdaptor<ValueTableFormat, Value> tableFormat;
+
+		public ValueFilterControl(final JFrame jFrame, final EnumTableFormatAdaptor<ValueTableFormat, Value> tableFormat, final EventList<Value> eventList, final FilterList<Value> filterList, final Map<String, List<Filter>> filters) {
+			super(jFrame, NAME, eventList, filterList, filters);
+			this.tableFormat = tableFormat;
+		}
+
+		@Override
+		protected Object getColumnValue(final Value value, final String column) {
+			ValueTableFormat format = ValueTableFormat.valueOf(column);
+			return format.getColumnValue(value);
+		}
+
+		@Override
+		protected EnumTableColumn<?> valueOf(final String column) {
+			return ValueTableFormat.valueOf(column);
+		}
+
+		@Override
+		protected List<EnumTableColumn<Value>> getColumns() {
+			return columnsAsList(ValueTableFormat.values());
+		}
+
+		@Override
+		protected List<EnumTableColumn<Value>> getShownColumns() {
+			return new ArrayList<EnumTableColumn<Value>>(tableFormat.getShownColumns());
+		}
+
+		@Override
+		protected void saveSettings(final String msg) {
+			program.saveSettings("Save ISK " + msg); //Save ISK Filters and Export Setttings
+		}
+	}
+
+	public static class TotalComparator implements Comparator<Value> {
+		@Override
+		public int compare(final Value o1, final Value o2) {
+			if (o1.isGrandTotal() && o2.isGrandTotal()) {
+				return 0;  //Equal (both StockpileTotal)
+			} else if (o1.isGrandTotal()) {
+				return 1;  //After
+			} else if (o2.isGrandTotal()) {
+				return -1; //Before
+			} else {
+				return 0;  //Equal (not StockpileTotal)
+			}
+		}
+	}
+	
+}
